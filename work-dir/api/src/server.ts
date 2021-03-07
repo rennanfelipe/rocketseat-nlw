cc8adae87fe7c7f84a3a05eb289617e4eb4a31f3
rennanfelipe <rennan.chaves95@gmail.com>
2021-03-07 17:11:11 -0300

    Gitignore

---
+++ work-dir/api/src/server.ts
@@ -0,0 +1,13 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.json({message: "Hello world!"});
+});
+
+app.post("/", (req, res) => {
+    return res.json({message: "Os dados foram salvos com sucesso!"});
+});
+
+app.listen(3000, () => console.log("It's alive! At 3000"));
